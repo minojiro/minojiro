df252729d7361e5e5dd02fd446aa5bc92a8962b0
minojiro <minojiro@users.noreply.github.com>
2021-12-19 13:21:04 +0900

    fix: microCMSのアップデートに伴う修正

--- api/client.ts
+++ api/client.ts
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
 export const microcmsClient = axios.create({
-  baseURL: 'https://minojiro.microcms.io/api/v1/',
-  timeout: 1000,
+  baseURL: "https://minojiro.microcms.io/api/v1/",
+  timeout: 2000,
   headers: {
-    'X-API-KEY': process.env.X_API_KEY,
+    "X-MICROCMS-API-KEY": process.env.X_API_KEY,
   },
 })
 
