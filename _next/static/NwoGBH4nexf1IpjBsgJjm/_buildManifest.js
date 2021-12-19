51ffb3324ada44a4a7ff9481b164ce620f7e6b48
minojiro <minojiro@users.noreply.github.com>
2021-12-19 04:21:55 +0000

    deploy: df252729d7361e5e5dd02fd446aa5bc92a8962b0

---
+++ _next/static/NwoGBH4nexf1IpjBsgJjm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-840768d7c8726903.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
