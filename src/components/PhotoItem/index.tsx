2a0b60f4621f65605336d4b251dd042a0114656f
minojiro <minojiro@users.noreply.github.com>
2023-03-05 15:07:03 +0900

    perf: webpを使用

--- src/components/PhotoItem/index.tsx
+++ src/components/PhotoItem/index.tsx
@@ -4,12 +4,15 @@ import type { PhotoPost } from '../../types'
 export default component$(({ item }: {item: PhotoPost}) => {
   return (
     <section>
-      <img
-        src={item.src}
-        alt=""
-        class="object-contain w-full h-auto aspect-photo bg-gray-50"
-        loading="lazy"
-      />
+      <picture>
+        <source srcSet={`${item.src}?fm=webp`} type="image/webp" />
+        <img
+          src={item.src}
+          alt=""
+          class="object-contain w-full h-auto aspect-photo bg-gray-50"
+          loading="lazy"
+        />
+      </picture>
       <div class="mt-6 px-5 md:px-0">
         <h2 class="font-bold mb-1">{item.modelName}</h2>
         <p class="text-sm">{item.staff}</p>
