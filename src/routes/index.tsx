61bf0c8a23a191dc14fd5cf20d9cba9bcf0be8be
minojiro <minojiro@users.noreply.github.com>
2023-03-21 16:38:22 +0900

    refactor: ヘッダーを別コンポーネントに移動

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,52 +1,58 @@
-import { component$, useSignal, useTask$, $, useOnWindow } from '@builder.io/qwik';
-import PhotoItem from '../components/PhotoItem';
-import type { DocumentHead } from '@builder.io/qwik-city';
-import type { PhotoPost } from '../types';
-import {getPhotoPosts} from '../api/photos'
+import {
+  component$,
+  useSignal,
+  useTask$,
+  $,
+  useOnWindow,
+} from "@builder.io/qwik";
+import PhotoItem from "../components/PhotoItem";
+import GlobalHeader from "../components/GlobalHeader";
+import type { DocumentHead } from "@builder.io/qwik-city";
+import type { PhotoPost } from "../types";
+import { getPhotoPosts } from "../api/photos";
 
-const MIN_SCROLL_TOP = 50
+const MIN_SCROLL_TOP = 50;
 
 export default component$(() => {
-  const photoPosts = useSignal<PhotoPost[]>([])
-  const isToUp = useSignal(true)
-  const lastScrollY = useSignal(0)
+  const photoPosts = useSignal<PhotoPost[]>([]);
+  const isToUp = useSignal(true);
+  const lastScrollY = useSignal(0);
   useTask$(async () => {
-    photoPosts.value = await getPhotoPosts()
+    photoPosts.value = await getPhotoPosts();
   });
   useOnWindow(
-    'scroll',
+    "scroll",
     $(() => {
-      const { scrollY } = window
-      isToUp.value = Math.max(MIN_SCROLL_TOP,lastScrollY.value) > scrollY
-      lastScrollY.value = scrollY
+      const { scrollY } = window;
+      isToUp.value = Math.max(MIN_SCROLL_TOP, lastScrollY.value) > scrollY;
+      lastScrollY.value = scrollY;
     })
-  )
+  );
   return (
-    <div class="max-w-5xl mx-auto text-gray-800 max-w-photoContainer">
-      <header class={`py-10 sticky top-0 md:py-24 ${isToUp.value ? '' : 'collapse'}`}>
-        <div class="px-5 md:px-0">
-          <h1 class="font-bold">minoJiro photographs</h1>
-          <p>contact me on <a href="https://www.instagram.com/the_minojiro/" rel="noopener" target="_blank" class="underline">Instagram</a>.</p>
-        </div>
-      </header>
-      <main class={`${isToUp.value ? '' : 'text-gray-300'}`}>
+    <div class="max-w-5xl mx-auto max-w-photoContainer text-gray-800 font-handwrite">
+      <GlobalHeader isToUp={isToUp.value} />
+      <main class={`${isToUp.value ? "" : "text-gray-300"}`}>
         <div class="flex flex-col gap-24">
-          {photoPosts.value.map((item) => (<PhotoItem key={item.id} item={item} />))}
+          {photoPosts.value.map((item) => (
+            <PhotoItem key={item.id} item={item} />
+          ))}
         </div>
       </main>
       <footer class="my-24 px-5 md:px-0">
-        <p class="text-gray-300"><small>© minojiro ALL RIGHTS RESERVED</small></p>
+        <p class="text-gray-300">
+          <small>© minojiro ALL RIGHTS RESERVED</small>
+        </p>
       </footer>
     </div>
   );
 });
 
 export const head: DocumentHead = {
-  title: 'minoJiro photographs',
+  title: "minoJiro photographs",
   meta: [
     {
-      name: 'description',
-      content: 'minoJiro photographs',
+      name: "description",
+      content: "minoJiro photographs",
     },
   ],
 };
