c9d85684c07f47d3ce95fc191857515f28e6a965
minojiro <minojiro@users.noreply.github.com>
2023-03-21 16:44:42 +0900

    fix: bodyタグの言語設定

--- src/root.tsx
+++ src/root.tsx
@@ -1,9 +1,18 @@
-import { component$, useTask$, useBrowserVisibleTask$,useSignal } from '@builder.io/qwik';
-import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
-import { initializeGtm } from './lib/gtm';
-import { getPhotoPosts } from './api/photos';
+import {
+  component$,
+  useTask$,
+  useBrowserVisibleTask$,
+  useSignal,
+} from "@builder.io/qwik";
+import {
+  QwikCityProvider,
+  RouterOutlet,
+  ServiceWorkerRegister,
+} from "@builder.io/qwik-city";
+import { initializeGtm } from "./lib/gtm";
+import { getPhotoPosts } from "./api/photos";
 
-import './global.css';
+import "./global.css";
 
 export default component$(() => {
   /**
@@ -12,13 +21,13 @@ export default component$(() => {
    *
    * Dont remove the `<head>` and `<body>` elements.
    */
-  const ogImage = useSignal('')
-  const siteName = 'minoJiro （みのじろー）の写真作品集'
+  const ogImage = useSignal("");
+  const siteName = "minoJiro （みのじろー）の写真作品集";
   useTask$(async () => {
-    const [photo] = await getPhotoPosts()
-    ogImage.value = photo?.src || ''
-  })
-  useBrowserVisibleTask$(() => initializeGtm())
+    const [photo] = await getPhotoPosts();
+    ogImage.value = photo?.src || "";
+  });
+  useBrowserVisibleTask$(() => initializeGtm());
 
   return (
     <QwikCityProvider>
@@ -32,15 +41,19 @@ export default component$(() => {
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://minojiro.com" />
         <meta property="og:image" content={ogImage.value} />
-        <meta property="og:description" content={siteName}/>
+        <meta property="og:description" content={siteName} />
         <meta property="og:locale" content="ja_JP" />
         <meta name="description" content={siteName} />
         <link rel="icon" type="image/svg+xml" href="/favicon.ico" />
       </head>
-      <body lang="ja" class="font-base text-lg">
+      <body lang="en" class="font-base text-lg">
         <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WNTDKBF"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WNTDKBF"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
         </noscript>
         <RouterOutlet />
         <ServiceWorkerRegister />
