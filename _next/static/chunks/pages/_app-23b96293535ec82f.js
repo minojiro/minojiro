219eaf78a5570235b4bfa8dd9b31140d7e84ebbc
minojiro <minojiro@users.noreply.github.com>
2025-01-23 12:27:55 +0000

    deploy: 47b0ef43cbdf0d3cb7e38135b331ece60bdaa9d4

---
+++ _next/static/chunks/pages/_app-23b96293535ec82f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[255],{4454:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(8153)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(4454),n(2923))),_N_E=_.O()}]);
